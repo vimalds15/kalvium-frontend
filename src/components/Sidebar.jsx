234a347e166f97452f7e117e503b6ea428eb3afb
vimalds15 <antovimalands@gmail.com>
2023-08-26 15:48:23 +0530

    some styling changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { sidebarItems } from "../constants/Sidebar";
 
 const Sidebar = ({ handleItemClick, activeItem }) => {
   return (
-    <div className="hidden md:flex flex-col justify-around w-[25%] mt-16 border bg-white border-r-neutral-200 fixed top-0 left-0 h-[95%] z-10">
+    <div className="hidden md:flex flex-col justify-around w-[25%] lg:w-[20%] mt-16 border bg-white border-r-neutral-200 fixed top-0 left-0 h-[95%] z-10">
       <div className="px-6 -mt-7 flex flex-col gap-4">
         {sidebarItems.map((element) => (
           <Link
