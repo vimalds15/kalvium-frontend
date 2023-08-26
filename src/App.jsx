8a1d32efe1e733f6aee4d28069b9cb3d643a5d17
vimalds15 <antovimalands@gmail.com>
2023-08-26 10:17:46 +0530

    implemented sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainLayout from './layouts/Main.layout'
+import { routes } from './routes/routes'
 
 function App() {
 
   return (
     <div className='App'>
       <MainLayout />
+
+      <div className="flex-1 md:ml-[20%] mt-14 p-10 overflow-y-auto">
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/constants/Sidebar.js
@@ -0,0 +1,17 @@
+import {
+    MdOutlineHome,
+    MdOutlineAssignment,
+} from "react-icons/md"
+
+export const sidebarItems=[
+    {
+        path:'/',
+        name:"Home",
+        icon:MdOutlineHome
+    },
+    {
+        path:'/attendance',
+        name:"Attendance",
+        icon:MdOutlineAssignment
+    },
+]
