433961aa6990d2372bfc07fc178bcad00f9039a7
vimalds15 <antovimalands@gmail.com>
2023-08-26 10:04:41 +0530

    added routes for home and attendance pages

---
+++ src/routes/routes.jsx
@@ -0,0 +1,12 @@
+import {HomePage,AttendancePage} from "../pages"
+
+export const routes = [
+    {
+        path:"/",
+        element:<HomePage />
+    },
+    {
+        path:"/attendance",
+        element:<AttendancePage />
+    },
+]
