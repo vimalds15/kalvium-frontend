abb1ea87fe0393f53b399420891080682ddf26e7
vimalds15 <antovimalands@gmail.com>
2023-08-26 22:29:17 +0530

    changed navbar logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import UserLogo from "../assets/user.png";
+import Logo from "../assets/logo.png";
 
 const MainNavBar = () => {
 
   return (
     <div className="flex justify-center h-16 px-10 py-6 bg-white shadow-md border border-b-neutral-200 z-10">
-      <div className="flex  items-center justify-between  w-full 2xl:max-w-6xl">
-        <div className="flex items-center">
+      <div className="flex  items-center justify-between md:justify-center  w-full 2xl:max-w-6xl">
+        <div className="flex items-center justify-center">
           <div className="mr-5">
-            <img src={UserLogo} className="h-10 w-10" alt="" />
+            <img src={Logo} className="h-14" alt="" />
           </div>
           <div className="flex flex-col justify-center">
-            <p className="hidden md:block font-semibold mt-[-2px]">
-              Hii
+            <p className="md:block font-semibold mt-[-2px]">
+              Attendance Portal
             </p>
           </div>
         </div>
