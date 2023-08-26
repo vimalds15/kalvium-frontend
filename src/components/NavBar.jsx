60ffcb0ba0754985108868e611b2602572e87e49
vimalds15 <antovimalands@gmail.com>
2023-08-26 10:00:19 +0530

    implemented navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import UserLogo from "../assets/user.png";
+
+const MainNavBar = () => {
+
+  return (
+    <div className="flex justify-center h-16 px-10 py-6 bg-white shadow-md border border-b-neutral-200  ">
+      <div className="flex  items-center justify-between  w-full 2xl:max-w-6xl">
+        <div className="flex items-center">
+          <div className="mr-5">
+            <img src={UserLogo} className="h-10 w-10" alt="" />
+          </div>
+          <div className="flex flex-col justify-center">
+            {/* <p className="text-sm text-gray-400 ">FULL STACK DEVELOPER</p> */}
+            <p className="hidden md:block font-semibold mt-[-2px]">
+              Hii
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainNavBar;
